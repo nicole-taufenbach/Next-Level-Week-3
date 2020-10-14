15f0063cc70d18254a7ab44a98bc7861e8e2890d
nicole-taufenbach <ntaufenbach@hotmail.com>
2020-10-13 23:33:32 -0300

    Dia 2 concluído :)

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,41 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+//create map
+const map = L.map("mapid", options).setView([-27.2109325, -49.6448719], 15);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popAnchor: [170, 2],
+});
+
+//create and add marker
+L.marker([-27.2109325, -49.6448719], { icon }).addTo(map);
+
+//image gallery
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  //remover todas as classes .active
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+  
+  //selecionar a imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  //atualizar o container de imagem
+  imageContainer.src = image.src;
+
+  //adicionar de volta a classe .active para o botao clicado
+  button.classList.add("active");
+}
